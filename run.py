a6b285bf31e99144b5df36016ab33b3ca0a95838
Joanna Gorska <gorska919@gmail.com>
2021-08-30 22:15:26 +0000

    Add function to validate email address inputed by the user

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import gspread
+import re
 from google.oauth2.service_account import Credentials
 
 
@@ -19,13 +20,33 @@ clients = SHEET.worksheet('clients')
 
 data = rooms.get_all_values()
 
+customer_email = ""
 def get_email_from_user():
     """
     Get email from the user
     """
+    global customer_email
     print("Please enter your email address")
     print("Example: email@domain.uk\n")
     customer_email = input("Enter your email here: ")
-    print(f"Email that you have provided is {customer_email}")
+    print(f"Email that you have provided is {customer_email}\n")
+    return customer_email
 
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+def validate_email(email):
+    """
+    Validates email input from the user
+    
+    """
+    if(re.fullmatch(regex, email)):
+        print("Valid Email")
+ 
+    else:
+        print("Please enter valid email address")
+        print("Example: email@domain.uk\n")
+
+# validate email code:
+# https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
 get_email_from_user()
+validate_email(customer_email)
