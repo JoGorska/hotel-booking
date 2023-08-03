38f6c5d21e8126aa2e4fbc7a6ee52cca9096eb8a
jogorska <gorska919@gmail.com>
2023-08-03 23:26:20 +0100

    Refactor json_creds into a string

--- controllers/default.js
+++ controllers/default.js
@@ -50,18 +50,18 @@ function socket() {
 }
 
 if (process.env.CREDS === true ) {
-    const json_creds = {
-        "type": process.env.type,
-        "project_id": process.env.project_id,
-        "private_key_id": process.env.private_key_id,
-        "private_key": process.env.private_key,
-        "client_email": process.env.client_email,
-        "client_id": process.env.client_id,
-        "auth_uri": process.env.auth_uri,
-        "token_uri": process.env.token_uri,
-        "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-        "client_x509_cert_url": process.env.client_x509_cert_url
-    }
+    const json_creds = `{
+        "type": ${process.env.type},
+        "project_id": ${process.env.project_id},
+        "private_key_id": ${process.env.private_key_id},
+        "private_key": ${process.env.private_key},
+        "client_email": ${process.env.client_email},
+        "client_id": ${process.env.client_id},
+        "auth_uri": ${process.env.auth_uri},
+        "token_uri": ${process.env.token_uri},
+        "auth_provider_x509_cert_url": ${process.env.auth_provider_x509_cert_url},
+        "client_x509_cert_url": ${process.env.client_x509_cert_url}
+    }`
     console.log("Creating creds.json file.");
     fs.writeFile('creds.json', json_creds, 'utf8', function (err) {
         if (err) {
